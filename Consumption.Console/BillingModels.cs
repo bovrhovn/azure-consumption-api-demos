d5c164373fd8804aafacc586567481a76c7919cc
Bojan Vrhovnik <bovrhovn@microsoft.com>
2020-09-21 16:40:02 +0200

    added call to get data about usage details - https://docs.microsoft.com/en-us/rest/api/consumption/#late-arriving-data

---
+++ Consumption.Console/BillingModels.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Consumption.Console
+{
+    public class Tags    {
+        [JsonProperty("web")]
+        public string Web; 
+
+        [JsonProperty("data")]
+        public string Data; 
+
+        [JsonProperty("ms-resource-usage")]
+        public string MsResourceUsage; 
+    }
+
+    public class ResourceProperty   
+    {
+        [JsonProperty("billingPeriodId")]
+        public string BillingPeriodId; 
+
+        [JsonProperty("usageStart")]
+        public DateTime UsageStart; 
+
+        [JsonProperty("usageEnd")]
+        public DateTime UsageEnd; 
+
+        [JsonProperty("instanceId")]
+        public string InstanceId; 
+
+        [JsonProperty("instanceName")]
+        public string InstanceName; 
+
+        [JsonProperty("instanceLocation")]
+        public string InstanceLocation; 
+
+        [JsonProperty("meterId")]
+        public string MeterId; 
+
+        [JsonProperty("usageQuantity")]
+        public double UsageQuantity; 
+
+        [JsonProperty("pretaxCost")]
+        public double PretaxCost; 
+
+        [JsonProperty("currency")]
+        public string Currency; 
+
+        [JsonProperty("additionalProperties")]
+        public string AdditionalProperties; 
+
+        [JsonProperty("isEstimated")]
+        public bool IsEstimated; 
+
+        [JsonProperty("subscriptionGuid")]
+        public string SubscriptionGuid; 
+
+        [JsonProperty("subscriptionName")]
+        public string SubscriptionName; 
+
+        [JsonProperty("product")]
+        public string Product; 
+
+        [JsonProperty("consumedService")]
+        public string ConsumedService; 
+
+        [JsonProperty("partNumber")]
+        public string PartNumber; 
+
+        [JsonProperty("resourceGuid")]
+        public string ResourceGuid; 
+
+        [JsonProperty("offerId")]
+        public string OfferId; 
+
+        [JsonProperty("chargesBilledSeparately")]
+        public bool ChargesBilledSeparately; 
+
+        [JsonProperty("meterDetails")]
+        public object MeterDetails; 
+    }
+
+    public class UsageResource    {
+        [JsonProperty("id")]
+        public string Id; 
+
+        [JsonProperty("name")]
+        public string Name; 
+
+        [JsonProperty("type")]
+        public string Type; 
+
+        [JsonProperty("tags")]
+        public Tags Tags; 
+
+        [JsonProperty("properties")]
+        public ResourceProperty Properties; 
+    }
+
+    public class BillingModel    {
+        [JsonProperty("value")]
+        public List<UsageResource> UsageList; 
+    }
+}
--- Consumption.Console/Program.cs
+++ Consumption.Console/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
 
 namespace Consumption.Console
 {
@@ -9,24 +14,44 @@ namespace Consumption.Console
         static async Task Main(string[] args)
         {
             System.Console.WriteLine("Authenticating against Azure...");
-            
+
             //authenticate against Azure
             //how to register application, is available here https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal
             string authority = "https://login.windows.net/" + Environment.GetEnvironmentVariable("TenantId");
             string clientId = Environment.GetEnvironmentVariable("ClientId");
             string secret = Environment.GetEnvironmentVariable("Secret");
             string resourceId = "https://management.azure.com/";
-            
-            var token = await AuthenticateAsync(authority,clientId,secret,resourceId);
+
+            var token = await AuthenticateAsync(authority, clientId, secret, resourceId);
             System.Console.WriteLine("Logged in with token - " + token.PasswordOrToken);
-            
+
             //token aquired - issue command to work with consumption API
-            
-            
+            string uri =
+                $"https://management.azure.com/subscriptions/{Environment.GetEnvironmentVariable("SubscriptionId")}/providers/Microsoft.Consumption/usageDetails?api-version=2018-03-31&$expand=properties/additionalProperties";
+            var (content, statusCode) = await GetAsync(uri, token.PasswordOrToken);
+            if (statusCode == HttpStatusCode.OK)
+            {
+                //convert to models
+                var billingModel = JsonConvert.DeserializeObject<BillingModel>(content);
+                System.Console.WriteLine($"Details: {Environment.NewLine}");
+                foreach (var usageResource in billingModel.UsageList)
+                {
+                    System.Console.WriteLine(
+                        $"{usageResource.Name} used in {usageResource.Properties.ConsumedService} " +
+                        $"{usageResource.Properties.UsageQuantity} {usageResource.Properties.Currency} " + "" +
+                        $"in {usageResource.Properties.SubscriptionName}");
+                }
+            }
+            else
+            {
+                System.Console.WriteLine("No data available!");
+            }
+
             System.Console.Read();
         }
-        
-        private static async Task<AuthData> AuthenticateAsync(string authority, string clientId,string secret,string resourceId)
+
+        private static async Task<AuthData> AuthenticateAsync(string authority, string clientId, string secret,
+            string resourceId)
         {
             var authContext = new AuthenticationContext(authority);
             var clientCred = new ClientCredential(clientId, secret);
@@ -38,7 +63,53 @@ namespace Consumption.Console
             var authData = new AuthData {Scheme = AuthScheme.BEARER, PasswordOrToken = result.AccessToken};
             return authData;
         }
+
+        private static async Task<(string Content, HttpStatusCode StatusCode)> GetAsync(string uri,
+            string authorizationHeader,
+            List<KeyValuePair<string, string>> additionalHeaders = null)
+        {
+            using var httpClient = new HttpClient();
+            HttpResponseMessage response = null;
+
+            httpClient.DefaultRequestHeaders.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json");
+            httpClient.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue("Bearer", authorizationHeader);
+
+            // All Cosmos Db calls expect the current UTC time in RFC1123 format and the API version in the header variables
+            if (uri.Contains("documents.azure.com"))
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue(authorizationHeader);
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(uri)
+            };
+
+            if (additionalHeaders != null)
+            {
+                foreach (var h in additionalHeaders)
+                {
+                    request.Headers.Add(h.Key, h.Value);
+                }
+            }
+
+            var content = "";
+
+            try
+            {
+                response = await httpClient.SendAsync(request);
+                content = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+            }
+            catch (Exception ex)
+            {
+                content += "\n\n" + ex.Message;
+            }
+
+            return (Content: content, StatusCode: response?.StatusCode ?? HttpStatusCode.InternalServerError);
+        }
     }
-    
-    
 }
